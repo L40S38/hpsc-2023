c9dcb65ebb6962b697731b5c92a311ce09ffeb02
L40S38 <you@example.com>
2023-04-17 10:57:00 +0900

    12,13 homework done

--- 02_openmp/13_merge_sort.cpp
+++ 02_openmp/13_merge_sort.cpp
@@ -24,8 +24,11 @@ void merge(std::vector<int>& vec, int begin, int mid, int end) {
 void merge_sort(std::vector<int>& vec, int begin, int end) {
   if(begin < end) {
     int mid = (begin + end) / 2;
+#pragma omp task shared(vec)
     merge_sort(vec, begin, mid);//left sort
+#pragma omp task shared(vec)
     merge_sort(vec, mid+1, end);//right sort
+#pragma omp taskwait
     merge(vec, begin, mid, end);
   }
 }
