2e76dd38a7085e3f0437bbab0c5027fb3c518a26
L40S38 <you@example.com>
2023-05-29 13:54:39 +0900

    interpolate pm3d

--- 14_pde/10_cavity.cpp
+++ 14_pde/10_cavity.cpp
@@ -1,150 +1,204 @@
 #include <iostream>
-#include <fstream>
 #include <vector>
 #include <cmath>
-using namespace std;
+#include <cstdio>
+#include <cstdlib>
 
-void plotData(const vector<vector<double>>& x, const vector<vector<double>>& y,
-              const vector<vector<double>>& p, const vector<vector<double>>& u,
-              const vector<vector<double>>& v) {
-    ofstream dataFile("data.txt");
-    if (!dataFile) {
-        cerr << "Failed to open data file." << endl;
-        return;
-    }
+// グローバル変数
+const int nx = 41;
+const int ny = 41;
+const int nt = 500;
+const int nit = 50;
+const double dx = 2.0 / (nx - 1);
+const double dy = 2.0 / (ny - 1);
+const double dt = 0.01;
+const double rho = 1.0;
+const double nu = 0.02;
 
-    int nx = x[0].size();
-    int ny = x.size();
-
-    // Write data to file
-    for (int i = 0; i < ny; ++i) {
-        for (int j = 0; j < nx; ++j) {
-            dataFile << x[i][j] << " " << y[i][j] << " " << p[i][j] << " " << u[i][j] << " " << v[i][j] << endl;
+// プロットデータをファイルに保存する関数
+void saveDataPressure(const std::vector<std::vector<double>>& data, const std::string& filename) {
+    FILE* fp = fopen(filename.c_str(), "w");
+    if (fp != nullptr) {
+        for (int i = 0; i < ny; i++) {
+            for (int j = 0; j < nx; j++) {
+                fprintf(fp, "%f ", data[i][j]);
+            }
+            fprintf(fp, "\n");
         }
-        dataFile << endl;
-    }
-    dataFile.close();
-
-    // Plot with gnuplot
-    FILE* gnuplotPipe = popen("gnuplot -persist", "w");
-    if (gnuplotPipe != nullptr) {
-        fprintf(gnuplotPipe, "set pm3d\n");
-        fprintf(gnuplotPipe, "set dgrid3d\n");
-        fprintf(gnuplotPipe, "splot 'data.txt' u 1:2:3 with pm3d title 'Pressure'\n");
-        fprintf(gnuplotPipe, "set arrow from 0,0 to 2,0 nohead\n");
-        fprintf(gnuplotPipe, "set arrow from 2,0 to 2,2 nohead\n");
-        fprintf(gnuplotPipe, "set arrow from 2,2 to 0,2 nohead\n");
-        fprintf(gnuplotPipe, "set arrow from 0,2 to 0,0 nohead\n");
-        fprintf(gnuplotPipe, "set title 'Velocity Field'\n");
-        fprintf(gnuplotPipe, "set xrange [0:2]\n");
-        fprintf(gnuplotPipe, "set yrange [0:2]\n");
-        fprintf(gnuplotPipe, "set xlabel 'X'\n");
-        fprintf(gnuplotPipe, "set ylabel 'Y'\n");
-        fprintf(gnuplotPipe, "plot 'data.txt' u 1:2:(sqrt($4*$4+$5*$5)) with vectors title 'Velocity'\n");
-        fflush(gnuplotPipe);
-        //fprintf(gnuplotPipe, "exit\n");
-        //pclose(gnuplotPipe);
+        fclose(fp);
     } else {
-        cerr << "Failed to open gnuplot pipe." << endl;
+        std::cerr << "Failed to open file: " << filename << std::endl;
     }
-    pclose(gnuplotPipe);
 }
 
-int main() {
-    int nx = 41;
-    int ny = 41;
-    int nt = 500;
-    int nit = 50;
-    double dx = 2.0 / (nx - 1);
-    double dy = 2.0 / (ny - 1);
-    double dt = 0.01;
-    double rho = 1.0;
-    double nu = 0.02;
+void saveDataVerocity(const std::vector<double>& x, const std::vector<double>& y,
+                        const std::vector<std::vector<double>>& u, const std::vector<std::vector<double>>& v,
+                        const std::string& filename) {
+    FILE* fp = fopen(filename.c_str(), "w");
+    if (fp != nullptr) {
+        for (int i = 0; i < ny; i++) {
+            for (int j = 0; j < nx; j++) {
+                fprintf(fp, "%f %f %f %f", x[i], y[j], u[i][j], v[i][j]);
+            }
+            fprintf(fp, "\n");
+        }
+        fclose(fp);
+    } else {
+        std::cerr << "Failed to open file: " << filename << std::endl;
+    }
+}
 
-    vector<double> x(nx);
-    vector<double> y(ny);
-    vector<vector<double>> u(ny, vector<double>(nx));
-    vector<vector<double>> v(ny, vector<double>(nx));
-    vector<vector<double>> p(ny, vector<double>(nx));
-    vector<vector<double>> b(ny, vector<double>(nx));
-    vector<vector<double>> X(ny, vector<double>(nx));
-    vector<vector<double>> Y(ny, vector<double>(nx));
+//境界条件
+void border(std::vector<std::vector<double>>& u, std::vector<std::vector<double>>& v) {
+    for (int i = 0; i < nx; i++) {
+        u[0][i] = 0.0;
+        u[ny - 1][i] = 1.0;
+        v[0][i] = 0.0;
+        v[ny - 1][i] = 0.0;
+    }
+    for (int i = 0; i < ny; i++) {
+        u[i][0] = 0.0;
+        u[i][nx - 1] = 0.0;
+        v[i][0] = 0.0;
+        v[i][nx - 1] = 0.0;
+    }
+}
 
-    // Generate meshgrid
-    for (int i = 0; i < nx; ++i) {
+// 初期条件の設定
+void initialize(std::vector<std::vector<double>>& u, std::vector<std::vector<double>>& v,
+                std::vector<std::vector<double>>& p, std::vector<std::vector<double>>& b) {
+    for (int i = 0; i < ny; i++) {
+        for (int j = 0; j < nx; j++) {
+            u[i][j] = 0.0;
+            v[i][j] = 0.0;
+            p[i][j] = 0.0;
+            b[i][j] = 0.0;
+        }
+    }
+    for (int i = 0; i < nx; i++) {
+        u[0][i] = 0.0;
+        u[ny - 1][i] = 0.0;
+        v[0][i] = 0.0;
+        v[ny - 1][i] = 0.0;
+    }
+    for (int i = 0; i < ny; i++) {
+        u[i][0] = 0.0;
+        u[i][nx - 1] = 0.0;
+        v[i][0] = 0.0;
+        v[i][nx - 1] = 0.0;
+    }
+}
+
+int main(void){
+    /*
+    X, Y = np.meshgrid(x, y)
+    は実装する必要あるのか？
+    */
+    // x軸とy軸
+    std::vector<double> x(nx);
+    std::vector<double> y(ny);
+    for (int i = 0; i < nx; i++) {
         x[i] = i * dx;
     }
-    for (int j = 0; j < ny; ++j) {
-        y[j] = j * dy;
-    }
-    for (int i = 0; i < nx; ++i) {
-        for (int j = 0; j < ny; ++j) {
-            X[j][i] = x[i];
-            Y[j][i] = y[j];
-        }
+    for (int i = 0; i < ny; i++) {
+        y[i] = i * dy;
     }
 
-    // Main loop
-    for (int n = 0; n < nt; ++n) {
-        for (int j = 1; j < ny - 1; ++j) {
-            for (int i = 1; i < nx - 1; ++i) {
-                b[j][i] = rho * (1 / dt * ((u[j][i + 1] - u[j][i - 1]) / (2 * dx) +
-                                           (v[j + 1][i] - v[j - 1][i]) / (2 * dy)) -
-                                 ((u[j][i + 1] - u[j][i - 1]) / (2 * dx)) * ((u[j][i + 1] - u[j][i - 1]) / (2 * dx)) -
-                                 2 * ((u[j + 1][i] - u[j - 1][i]) / (2 * dy)) *
-                                         ((v[j][i + 1] - v[j][i - 1]) / (2 * dx)) -
-                                 ((v[j + 1][i] - v[j - 1][i]) / (2 * dy)) * ((v[j + 1][i] - v[j - 1][i]) / (2 * dy)));
+    // 変数の初期化
+    std::vector<std::vector<double>> u(ny, std::vector<double>(nx));
+    std::vector<std::vector<double>> v(ny, std::vector<double>(nx));
+    std::vector<std::vector<double>> p(ny, std::vector<double>(nx));
+    std::vector<std::vector<double>> b(ny, std::vector<double>(nx));
+    initialize(u, v, p, b);
+
+    // gnuplotのパイプラインの作成
+    FILE* gnuplotPipe = popen("gnuplot -persist", "w");
+    if (gnuplotPipe == nullptr) {
+        std::cerr << "Failed to open gnuplot pipe." << std::endl;
+        return 1;
+    }
+
+    for(int n=0; n<nt; n++){
+        for(int j=1; j<ny-1; j++){
+            for(int i=1; i<nx-1; i++){
+                b[j][i] = rho * (1 / dt *
+                                 ((u[j][i+1] - u[j][i-1]) / (2 * dx) + (v[j+1][i] - v[j-1][i]) / (2 * dy)) -
+                                 ((u[j][i+1] - u[j][i-1]) / (2 * dx)) * ((u[j][i+1] - u[j][i-1]) / (2 * dx)) -
+                                 2 * ((u[j+1][i] - u[j-1][i]) / (2 * dy)) *
+                                     ((v[j][i+1] - v[j][i-1]) / (2 * dx)) -
+                                 ((v[j+1][i] - v[j-1][i]) / (2 * dy)) * ((v[j+1][i] - v[j-1][i]) / (2 * dy)));
             }
         }
-        for (int it = 0; it < nit; ++it) {
-            vector<vector<double>> pn = p;
-            for (int j = 1; j < ny - 1; ++j) {
-                for (int i = 1; i < nx - 1; ++i) {
+        for(int it=0; it<nit; it++){
+            std::vector<std::vector<double>> pn = p;
+            for(int j=1; j<ny-1; j++){
+                for(int i=1; i<nx-1; i++){
                     p[j][i] =
-                        (dy * dy * (pn[j][i + 1] + pn[j][i - 1]) + dx * dx * (pn[j + 1][i] + pn[j - 1][i]) -
+                        (dy * dy * (pn[j][i+1] + pn[j][i-1]) +
+                         dx * dx * (pn[j+1][i] + pn[j-1][i]) -
                          b[j][i] * dx * dx * dy * dy) /
                         (2 * (dx * dx + dy * dy));
                 }
             }
-            for (int i = 0; i < nx; ++i) {
+            for (int i = 0; i < nx; i++) {
                 p[0][i] = p[1][i];
                 p[ny - 1][i] = 0.0;
             }
-            for (int j = 0; j < ny; ++j) {
-                p[j][0] = p[j][1];
-                p[j][nx - 1] = p[j][nx - 2];
+            for (int i = 0; i < ny; i++) {
+                p[i][0] = p[i][1];
+                p[i][nx - 1] = p[i][nx - 2];
             }
         }
-        for (int j = 1; j < ny - 1; ++j) {
-            for (int i = 1; i < nx - 1; ++i) {
-                u[j][i] = u[j][i] - u[j][i] * dt / dx * (u[j][i] - u[j][i - 1]) -
-                          v[j][i] * dt / dy * (u[j][i] - u[j - 1][i]) -
-                          dt / (2 * rho * dx) * (p[j][i + 1] - p[j][i - 1]) +
-                          nu * dt / dx * dx * (u[j][i + 1] - 2 * u[j][i] + u[j][i - 1]) +
-                          nu * dt / dy * dy * (u[j + 1][i] - 2 * u[j][i] + u[j - 1][i]);
-                v[j][i] = v[j][i] - u[j][i] * dt / dx * (v[j][i] - v[j][i - 1]) -
-                          v[j][i] * dt / dy * (v[j][i] - v[j - 1][i]) -
-                          dt / (2 * rho * dx) * (p[j + 1][i] - p[j - 1][i]) +
-                          nu * dt / dx * dx * (v[j][i + 1] - 2 * v[j][i] + v[j][i - 1]) +
-                          nu * dt / dy * dy * (v[j + 1][i] - 2 * v[j][i] + v[j - 1][i]);
-            }
-        }
-        for (int i = 0; i < nx; ++i) {
-            u[0][i] = 0.0;
-            u[ny - 1][i] = 1.0;
-            v[0][i] = 0.0;
-            v[ny - 1][i] = 0.0;
-        }
-        for (int j = 0; j < ny; ++j) {
-            u[j][0] = 0.0;
-            u[j][nx - 1] = 0.0;
-            v[j][0] = 0.0;
-            v[j][nx - 1] = 0.0;
-        }
+        std::vector<std::vector<double>> un = u;
+        std::vector<std::vector<double>> vn = v;
+        for (int j=1; j<ny-1; j++) {
+            for (int i=1; i<nx-1; i++) {
+                u[j][i] = un[j][i] - un[j][i] * dt / dx * (un[j][i] - un[j][i-1]) -
+                                    vn[j][i] * dt / dy * (un[j][i] - un[j-1][i]) -
+                                    dt / (2 * rho * dx) * (p[j][i+1] - p[j][i-1]) +
+                                    nu * dt / (dx * dx) * (un[j][i+1] - 2 * un[j][i] + un[j][i-1]) +
+                                    nu * dt / (dy * dy) * (un[j+1][i] - 2 * un[j][i] + un[j-1][i]);
 
-        // Output data for plotting
-        plotData(X, Y, p, u, v);
+                v[j][i] = vn[j][i] - vn[j][i] * dt / dx * (vn[j][i] - vn[j][i-1]) -
+                                    vn[j][i] * dt / dy * (vn[j][i] - vn[j-1][i]) -
+                                    dt / (2 * rho * dx) * (p[j+1][i] - p[j-1][i]) +
+                                    nu * dt / (dx * dx) * (vn[j][i+1] - 2 * vn[j][i] + vn[j][i-1]) +
+                                    nu * dt / (dy * dy) * (vn[j+1][i] - 2 * vn[j][i] + vn[j-1][i]);
+            }
+        }
+        border(u,v);
+
+        // プロットデータをファイルに保存
+        saveDataPressure(p, "pressure.dat");
+        //saveData(u, "velocity_u.dat");
+        //saveData(v, "velocity_v.dat");
+        saveDataVerocity(x,y,u,v, "verosity.dat");
+        /*
+        plt.contourf(X, Y, p, alpha=0.5, cmap=plt.cm.coolwarm)
+        plt.quiver(X[::2, ::2], Y[::2, ::2], u[::2, ::2], v[::2, ::2])
+        plt.pause(.01)
+        plt.clf()
+        */
+
+        // gnuplotにプロットコマンドを送信
+        //fprintf(gnuplotPipe, "set title 'Pressure'\n");
+        fprintf(gnuplotPipe, "set xrange [0:2]\n");
+        fprintf(gnuplotPipe, "set yrange [0:2]\n");
+        fprintf(gnuplotPipe, "set isosamples 1000\n");
+        fprintf(gnuplotPipe, "set view map\n");
+        fprintf(gnuplotPipe, "set pm3d interpolate 15,15\n");
+        //fprintf(gnuplotPipe, "set cbrange [-1.0:1.0]\n");
+        fprintf(gnuplotPipe, "splot 'pressure.dat' matrix with pm3d\n");
+        //fprintf(gnuplotPipe, "plot 'verosity.dat' u 1:2:3:4  with vector\n");
+        fprintf(gnuplotPipe, "pause .01\n");
+        fflush(gnuplotPipe);
+
+        // 一時停止
+        std::cout << "Step: " << n + 1 << " / " << nt << std::endl;
     }
+
+    // gnuplotパイプラインを閉じる
+    pclose(gnuplotPipe);
 
     return 0;
 }
